c0bffc092f64fa18d128dc28154f9e1320c96b36
samuelbarata <oliveira3@icloud.com>
2019-04-05 11:34:35 +0100

    modifiquei os tabs

--- proj001.asm
+++ proj001.asm
@@ -40,13 +40,13 @@ SP_init:
 
 PLACE		0
 inicializacao:
-		MOV	SP 	SP_init
+	MOV	SP 	SP_init
 
 
 main:
 	CALL	teclado
 
-
+	
 	JMP		main
 
 
@@ -74,47 +74,46 @@ teclado:
 	PUSH	R8
 
 
-	MOV R5, TEC_IN	; R5 com endereço de memória BUFFER 
-	MOV	R1, LINHA	; testar a linha 
-	MOV	R2, PINPOUT	; R2 com o endereço do periférico
-	MOV R6, tec_out ; Onde se guarda o output do teclado
-	MOV R7, -1		; Valor caso nenhuma tecla seja primida
+	MOV 	R5, 	TEC_IN	; R5 com endereço de memória BUFFER 
+	MOV		R1, 	LINHA	; testar a linha 
+	MOV		R2, 	PINPOUT	; R2 com o endereço do periférico
+	MOV 	R6, 	tec_out ; Onde se guarda o output do teclado
+	MOV 	R7, 	-1		; Valor caso nenhuma tecla seja primida
 
-	SHR		R1, 1		; linha do teclado, passa para a seguinte [anterior]
-	JZ		load		; se chegou a 0 volta à 1ª
-	MOVB 	[R2], R1	; input teclado
-	MOVB 	R3, [R2]	; output teclado
-	AND 	R3, R3		; afectar as flags (MOVs não afectam as flags) - verifica se alguma tecla foi pressionada
-	JZ 		store		; nenhuma tecla premida
-	MOV		R4, R3		; guardar tecla premida em registo
+	SHR		R1, 	1		; linha do teclado, passa para a seguinte [anterior]
+	JZ		load			; se chegou a 0 volta à 1ª
+	MOVB 	[R2],	R1		; input teclado
+	MOVB 	R3, 	[R2]	; output teclado
+	AND 	R3,		R3		; afectar as flags (MOVs não afectam as flags) - verifica se alguma tecla foi pressionada
+	JZ 		store			; nenhuma tecla premida
+	MOV		R4, 	R3		; guardar tecla premida em registo
 
 
-	MOV 	R7, 0		; contador linhas
-	MOV 	R8, 0		; contador colunas
+	MOV 	R7, 	0		; contador linhas
+	MOV 	R8, 	0		; contador colunas
 
 	linhas:
-	CMP		R1, 1		; verifica se o bit de menor peso é 1
-	JZ		colunas		; se for vai avaliar a mesma coisa nas linhas
-	ADD		R7, 1		; se n for adiciona 1 ao contador
-	SHR		R1, 1		; desloca o numero para a direita
-	JMP		linhas		; repete até determinar o nº de linhas
+	CMP		R1, 	1		; verifica se o bit de menor peso é 1
+	JZ		colunas			; se for vai avaliar a mesma coisa nas linhas
+	ADD		R7, 	1		; se n for adiciona 1 ao contador
+	SHR		R1, 	1		; desloca o numero para a direita
+	JMP		linhas			; repete até determinar o nº de linhas
 
 	colunas:
-	CMP		R4, 1
+	CMP		R4, 	1
 	JZ		equacao
-	ADD		R8, 1
-	SHR		R4, 1
+	ADD		R8, 	1
+	SHR		R4, 	1
 	JMP		colunas	
 
-	equacao:			; R7 = 4*linha + coluna
-	SHL		R7, 2		; linha * 4
-	ADD		R7, R8		; (linha*4) + coluna
+	equacao:				; R7 = 4*linha + coluna
+	SHL		R7, 	2		; linha * 4
+	ADD		R7, 	R8		; (linha*4) + coluna
 
 	store:
-	MOV 	[R6],R7
+	MOV 	[R6],	R7
 
 
-;#######################################################################
 	fim:
 	POP 	R8
 	POP		R7
