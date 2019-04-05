e525f462e4be6cee821b606214a0ac188ce8dbd9
samuelbarata <oliveira3@icloud.com>
2019-04-05 22:34:46 +0100

    testei o teclado, acho que está a gravar na memoria [escreve 20 quando n se carrega em nada]; n consigo mostrar no diplay debug

--- proj001.asm
+++ proj001.asm
@@ -14,39 +14,49 @@
 ; │ Constantes															│
 ; ╰─────────────────────────────────────────────────────────────────────╯
 
-DISPLAY1		EQU 0A000H  ; (periférico POUT-1)
-DISPLAY2		EQU 0C000H  ; (periférico POUT-2)
-DISPLAY3		EQU	06000H	; (periférico POUT-3)
-TEC_IN			EQU 0E000H  ; endereço do teclado (periférico PIN)
-PSCREEN			EQU 8000H   ; endereço do ecrã (pixelscreen)
+DISPLAY1		EQU 0A000H  ; Displays hexa			(periférico POUT-1)
+TEC_IN			EQU 0C000H  ; Input teclado			(periférico POUT-2)
+DISPLAY2		EQU	06000H	; Displays hexa extra	(periférico POUT-3)
+TEC_OUT			EQU 0E000H  ; endereço do teclado	(periférico PIN)
+PSCREEN			EQU 8000H   ; endereço do ecrã		(pixelscreen)
 LINHA			EQU	16		; linha to teclado a testar primeiro
 
 
 
 PLACE		1000H
 
-tec_out:	WORD 0
+key_press:	WORD	0
 
-submarino:	
-			STRING 0,0,1,1,0
-			STRING 0,0,0,1,0
-			STRING 1,1,1,1,1
+submarino:	STRING	5,3,0,0		;largura, altura, posição x, y
+			STRING	0,0,1,1,0
+			STRING	0,0,0,1,0
+			STRING	1,1,1,1,1
 
-SP_f:		TABLE 100H
-SP_init:
+SP_final:	TABLE	100H
+SP_inicial:
 
 
 
 
 PLACE		0
 inicializacao:
-	MOV	SP 	SP_init
-
+	MOV		SP,		SP_inicial
+	MOV		R0,		0
+	MOV		R1,		0
+	MOV		R2,		0
+	MOV		R3,		0
+	MOV		R4,		0
+	MOV		R5,		0
+	MOV		R6,		0
+	MOV		R7,		0
+	MOV		R8,		0
+	MOV		R9,		0
+	MOV		R10,	0
 
 main:
 	CALL	teclado
 
-	
+
 	JMP		main
 
 
@@ -62,8 +72,7 @@ main:
 ; ╰─────────────────────────────────────────────────────────────────────╯
 
 teclado:
-	init:
-	PUSH	R0
+  init:
 	PUSH	R1
 	PUSH	R2
 	PUSH	R3
@@ -73,48 +82,50 @@ teclado:
 	PUSH	R7
 	PUSH	R8
 
+	MOV 	R5, 	TEC_IN	; R5 com endereço de memória Input teclado 
+	MOV		R1, 	LINHA	; testar a linha
+	MOV		R2, 	TEC_OUT	; R2 com o endereço do periférico
+	MOV 	R6, 	key_press	; Onde se guarda o output do teclado
+	MOV 	R7, 	20H		; Valor caso nenhuma tecla seja primida
+	MOV		R0,		DISPLAY1; DEBUG VALOR NO DISPLAY																	#
 
-	MOV 	R5, 	TEC_IN	; R5 com endereço de memória BUFFER 
-	MOV		R1, 	LINHA	; testar a linha 
-	MOV		R2, 	PINPOUT	; R2 com o endereço do periférico
-	MOV 	R6, 	tec_out ; Onde se guarda o output do teclado
-	MOV 	R7, 	-1		; Valor caso nenhuma tecla seja primida
-
-	SHR		R1, 	1		; linha do teclado, passa para a seguinte [anterior]
-	JZ		load			; se chegou a 0 volta à 1ª
-	MOVB 	[R2],	R1		; input teclado
+  ciclo:
+ 	SHR		R1, 	1		; linha do teclado, passa para a seguinte [anterior]
+ 	JZ		store			; Se estiver a 0 significa que nenhuma das teclas foi primida e guarda -1 na memória
+	MOVB 	[R5],	R1		; input teclado
 	MOVB 	R3, 	[R2]	; output teclado
 	AND 	R3,		R3		; afectar as flags (MOVs não afectam as flags) - verifica se alguma tecla foi pressionada
-	JZ 		store			; nenhuma tecla premida
+	JZ 		ciclo			; nenhuma tecla premida
 	MOV		R4, 	R3		; guardar tecla premida em registo
-
 
 	MOV 	R7, 	0		; contador linhas
 	MOV 	R8, 	0		; contador colunas
 
-	linhas:
+  linhas:
 	CMP		R1, 	1		; verifica se o bit de menor peso é 1
 	JZ		colunas			; se for vai avaliar a mesma coisa nas linhas
 	ADD		R7, 	1		; se n for adiciona 1 ao contador
 	SHR		R1, 	1		; desloca o numero para a direita
 	JMP		linhas			; repete até determinar o nº de linhas
 
-	colunas:
+  colunas:
 	CMP		R4, 	1
 	JZ		equacao
 	ADD		R8, 	1
 	SHR		R4, 	1
 	JMP		colunas	
 
-	equacao:				; R7 = 4*linha + coluna
+  equacao:				; R7 = 4*linha + coluna
 	SHL		R7, 	2		; linha * 4
 	ADD		R7, 	R8		; (linha*4) + coluna
 
-	store:
-	MOV 	[R6],	R7
+	MOV		[R0],	R7		;DEBUG																						#
 
+  store:
+	MOVB 	[R6],	R7
+	
 
-	fim:
+  fim:
 	POP 	R8
 	POP		R7
 	POP		R6
@@ -123,5 +134,7 @@ teclado:
 	POP		R3
 	POP		R2
 	POP		R1
-	POP		R0
 	RET
+
+
+
